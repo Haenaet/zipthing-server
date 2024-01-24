a95638820f6e5b2015fe4e37686101642c0d0e65
yujinkim1 <yujin7621@me.com>
2024-01-24 23:54:17 +0900

    chore: configuration 수정

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -7,17 +7,22 @@ import Vapor
 public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-
-    app.databases.use(DatabaseConfigurationFactory.mysql(
-        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? MySQLConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-        database: Environment.get("DATABASE_NAME") ?? "vapor_database"
-    ), as: .mysql)
-
-    app.migrations.add(CreateTodo())
+   
+    // set database
+    try setFluentMySQL(app)
 
     // register routes
     try routes(app)
+}
+
+private func setFluentMySQL(_ app: Application) throws {
+    app.databases.use(
+        DatabaseConfigurationFactory.mysql(
+            hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+            port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? MySQLConfiguration.ianaPortNumber,
+            username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
+            password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
+            database: Environment.get("DATABASE_NAME") ?? "vapor_database"
+        ), as: .mysql
+    )
 }
