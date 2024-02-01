3f7be0e829eb69ebd4f4360af9f833da1ba2c2b2
yujinkim1 <yujin7621@me.com>
2024-02-01 23:53:39 +0900

    chore: Apply clean architecture

--- Package.swift
+++ Package.swift
@@ -7,33 +7,53 @@ let package = Package(
        .macOS(.v13)
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.83.1"),
         .package(url: "https://github.com/vapor/fluent.git", from: "4.8.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.83.1"),
         .package(url: "https://github.com/vapor/fluent-mysql-driver.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor-community/Imperial.git", from: "1.0.0"),
         .package(url: "https://github.com/vapor/jwt.git", from: "4.0.0")
     ],
     targets: [
+        /// Run Module
         .executableTarget(
+            name: "Run",
+            dependencies: ["App"]
+        ),
+        /// App Module
+        .target(
             name: "App",
             dependencies: [
                 .product(name: "Fluent", package: "fluent"),
-                .product(name: "FluentMySQLDriver", package: "fluent-mysql-driver"),
                 .product(name: "Vapor", package: "vapor"),
-                .product(name: "ImperialGitHub", package: "Imperial"),
-                .product(name: "JWT", package: "jwt"),
+                "Data",
             ]
         ),
+        /// Data Module
+        .target(
+            name: "Data",
+            dependencies: [
+                .product(name: "Fluent", package: "fluent"),
+                .product(name: "Vapor", package: "vapor"),
+                .product(name: "FluentMySQLDriver", package: "fluent-mysql-driver"),
+                "Domain",
+            ]
+        ),
+        /// Domain Module
+        .target(
+            name: "Domain",
+            dependencies: [
+                .product(name: "Fluent", package: "fluent"),
+                .product(name: "Vapor", package: "vapor"),
+            ]
+        ),
+        /// Test Module
         .testTarget(name: "AppTests", dependencies: [
             .target(name: "App"),
             .product(name: "XCTVapor", package: "vapor"),
 
             // Workaround for https://github.com/apple/swift-package-manager/issues/6940
-            .product(name: "Vapor", package: "vapor"),
             .product(name: "Fluent", package: "Fluent"),
-            .product(name: "FluentMySQLDriver", package: "fluent-mysql-driver"),
-            .product(name: "ImperialGitHub", package: "Imperial"),
-            .product(name: "JWT", package: "jwt"),
+            .product(name: "Vapor", package: "vapor"),
         ])
     ]
 )
